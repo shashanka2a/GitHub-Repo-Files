2b346d0910c351a74e200670d93898e69837d836
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-23 15:56:06 +0530

    Update test.sh
    adding comments

--- test.sh
+++ test.sh
@@ -3,14 +3,19 @@ folder="LeetCode"
 foldercopy="LeetCode-Copy"
 git clone $url
 cd $folder
+#Stores the files present in directory line by line 
 ls > files.txt
+# Prints the number of files
 wc -l files.txt
 cd ..
 mkdir $foldercopy
 for f in $folder/*
 do 
+	#moves the files [-v for verbose -u for update(keeps only one copy)]
 	mv -v -u $f ./$foldercopy/
 done
+#rmdir works fine for deleting empty directory
+# -rf to force remove the hidden files
 rm -rf $folder
 
 
